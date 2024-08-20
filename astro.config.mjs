d75427ead17773fb7595f56ef6056bb64dc4a340
SiZapPaaiGwat <xugaofan@gmail.com>
2024-08-20 13:59:52 +0800

    删除国际化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
@@ -9,25 +8,24 @@ import { CONFIG, LANGUAGES_CODE } from "./src/lib/config";
 // https://astro.build/config
 export default defineConfig({
   site: CONFIG.website,
-  trailingSlash: 'ignore',
+  trailingSlash: "ignore",
   compressHTML: false,
   i18n: {
     defaultLocale: "en",
     locales: CONFIG.locals,
     routing: {
-        prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
-  integrations: [tailwind(), react(), sitemap({
-    i18n: {
-      defaultLocale: "en",
-      locales: LANGUAGES_CODE
-    }
-  })],
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: LANGUAGES_CODE,
+      },
+    }),
+  ],
+  output: "static",
 });
