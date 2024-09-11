57dafb5913e996517e0ed11c4f6df27747783a6e
dingan <374619995@qq.com>
2024-09-11 19:20:06 +0800

    style

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -97,7 +97,7 @@ export default function PricingCard() {
 								<span>{product.credits} Credits</span>
 							</li>
 						</ul>
-						<div className='flex  flex-col gap-2'>
+						<div className='flex flex-col gap-2'>
 							<a
 								href='#'
 								className='text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900'
@@ -116,7 +116,7 @@ export default function PricingCard() {
 							</a>
 							<a
 								href='#'
-								className={`text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900 ${
+								className={`text-white  bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900 ${
 									creating && index === currentIndex && 'cursor-not-allowed'
 								}`}
 								onClick={() => {
