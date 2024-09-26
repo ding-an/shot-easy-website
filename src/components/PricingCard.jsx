c4d057173067ff9bb329c8619c14d5f14762bd21
饭食钢 <xugaofan@gmail.com>
2024-09-26 08:43:00 +0800

    Remove paddle

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -114,7 +114,7 @@ export default function PricingCard() {
 							>
 								Get started by Payssion
 							</a>
-							<a
+{/* 							<a
 								href='#'
 								className={`text-white  bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900 ${
 									creating && index === currentIndex && 'cursor-not-allowed'
@@ -131,7 +131,7 @@ export default function PricingCard() {
 								}}
 							>
 								{creating && index === currentIndex ? <Spin /> : 'Get started by Paddle'}
-							</a>
+							</a> */}
 						</div>
 					</div>
 				))}
