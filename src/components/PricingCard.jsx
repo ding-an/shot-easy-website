b892f5823a7ec6a8dcbbd1506b2ff4342cd5be23
dingan <374619995@qq.com>
2025-01-08 15:35:44 +0800

    feat:添加premium商品

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -18,6 +18,16 @@ const features = [
 		'24/7 Dedicated Customer Support',
 	],
 ]
+const features2 = [
+	['Individual configuration', 'No setup, or hidden fees', 'Credits'],
+	[
+		'Supports Up to 10 Devices/Users',
+		'No setup, or hidden fees',
+		'Credits',
+		'API Batch Processing',
+		'24/7 Dedicated Customer Support',
+	],
+]
 
 function FeatureItems({ product, index }) {
 	return (
@@ -104,10 +114,97 @@ function FeatureItems({ product, index }) {
 		</ul>
 	)
 }
+function FeatureItems2({ product, index }) {
+	return (
+		<ul role='list' className='mb-8 space-y-4 text-left text-sm'>
+			<li className='flex items-center space-x-3'>
+				<svg
+					className='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
+					fill='currentColor'
+					viewBox='0 0 20 20'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<path
+						fillRule='evenodd'
+						d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+						clipRule='evenodd'
+					></path>
+				</svg>
+				<span>{features2[index][0]}</span>
+			</li>
+			<li className='flex items-center space-x-3'>
+				<svg
+					className='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
+					fill='currentColor'
+					viewBox='0 0 20 20'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<path
+						fillRule='evenodd'
+						d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+						clipRule='evenodd'
+					></path>
+				</svg>
+				<span>{features2[index][1]}</span>
+			</li>
+			<li className='flex items-center space-x-3'>
+				<svg
+					className='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
+					fill='currentColor'
+					viewBox='0 0 20 20'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<path
+						fillRule='evenodd'
+						d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+						clipRule='evenodd'
+					></path>
+				</svg>
+				<span>{product.credits} Credits /month</span>
+			</li>
+			{features2[index][3] && (
+				<li className='flex items-center space-x-3'>
+					<svg
+						className='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
+						fill='currentColor'
+						viewBox='0 0 20 20'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path
+							fillRule='evenodd'
+							d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+							clipRule='evenodd'
+						></path>
+					</svg>
+					<span>{features2[index][3]}</span>
+				</li>
+			)}
+			{features2[index][4] && (
+				<li className='flex items-center space-x-3'>
+					<svg
+						className='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
+						fill='currentColor'
+						viewBox='0 0 20 20'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path
+							fillRule='evenodd'
+							d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+							clipRule='evenodd'
+						></path>
+					</svg>
+					<span>{features2[index][4]}</span>
+				</li>
+			)}
+		</ul>
+	)
+}
 
 export default function PricingCard() {
 	const [products, setProducts] = useState([])
+	const [preminumProducts, setPremiumProducts] = useState([])
 	const [product, setProduct] = useState(null)
+	const [activedKey, setActivedKey] = useState(1)
 	const handlers = useHandlers()
 	const { setVisible } = handlers
 
@@ -134,12 +231,36 @@ export default function PricingCard() {
 				}))
 			)
 		})
+		get(`/products`, { type: 0 }).then(({ data }) => {
+			const extraInfo = [
+				{
+					title: '1 month',
+					desc: 'Best option for personal use & for your next project.',
+				},
+				{
+					title: '12 months',
+					desc: 'Relevant for multiple users, extended & premium support.',
+				},
+			]
+			setPremiumProducts(
+				data.map((product, index) => ({
+					...product,
+					...(extraInfo[index] || {}),
+				}))
+			)
+		})
 	}, [])
 
 	return (
 		<>
+			<Tabs
+				activedKey={activedKey}
+				onClick={value => {
+					setActivedKey(value)
+				}}
+			/>
 			<div className='space-y-8 lg:grid lg:grid-cols-3 sm:gap-3 xl:gap-5 lg:space-y-0 min-h-[500px]'>
-				{products.map((product, index) => (
+				{(activedKey === 1 ? products : preminumProducts).map((product, index) => (
 					<div
 						key={product.id}
 						className='flex flex-col p-3 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-4 dark:bg-gray-800 dark:text-white'
@@ -151,7 +272,12 @@ export default function PricingCard() {
 								${product?.discount?.price || product.price}
 							</span>
 						</div>
-						<FeatureItems product={product} index={index} />
+						{activedKey === 1 ? (
+							<FeatureItems product={product} index={index} />
+						) : (
+							<FeatureItems2 product={product} index={index} />
+						)}
+
 						<div className='flex flex-col gap-2 mt-auto'>
 							<PaymentNet product={product} />
 							{/* <a
@@ -272,3 +398,31 @@ const Apple = () => (
 		</g>
 	</svg>
 )
+
+const Tabs = ({ activedKey, onClick }) => {
+	// 菜单
+	const tabs = [
+		{ label: 'Credits Package', value: 1 },
+		{ label: 'Premium Plan', value: 0 },
+	]
+	return (
+		<ul className='flex h-20 w-full space-x-6 font-bold text-3xl justify-center mb-4'>
+			{tabs.map(({ label, value }) => (
+				<li
+					key={value}
+					className={`relative flex h-full cursor-pointer items-center font-bold ${
+						activedKey === value ? 'text-white/96' : 'text-white/64'
+					}`}
+					onClick={() => {
+						onClick(value)
+					}}
+				>
+					{label}
+					{activedKey === value && (
+						<div className='absolute bottom-[10px] left-1/2 h-[6px] w-20 -translate-x-1/2 bg-primary-600'></div>
+					)}
+				</li>
+			))}
+		</ul>
+	)
+}
