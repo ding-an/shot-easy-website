95f043c57648a2853a68e87a5174edc328b2c730
K <31679674+HiStyx@users.noreply.github.com>
2025-01-22 15:44:37 +0800

    feat: 一些样式和配置修改

--- src/components/Products/Detail.tsx
+++ src/components/Products/Detail.tsx
@@ -19,7 +19,7 @@ const Detail = ({ products }) => {
 		<>
 			<div className='mt-6 flex flex-nowrap gap-3 overflow-x-auto xl:mt-0 xl:flex-1 xl:gap-5'>
 				{products.map(i => (
-					<div className='w-[152px] flex-shrink-0 rounded-2xl border border-solid border-white/10 bg-[#222b38] p-3 text-white/94 xl:h-[240px] xl:w-[200px] xl:max-w-[33%] xl:flex-1 xl:rounded-2.5xl xl:p-4'>
+					<div className='w-[152px] flex-shrink-0 rounded-2xl border border-solid border-white/10 bg-[#222b38] p-3 text-white/94 xl:h-[240px] xl:w-[200px] xl:max-w-[30%] xl:rounded-2.5xl xl:p-4'>
 						{i.discount && <div className={`inline-block ${tag2}`}>{i.discount} off</div>}
 						<div className='font-semibold xl:my-3 xl:text-base'>{i.short_name}</div>
 						<div className='font-semibold xl:text-2xl'>{i.price}</div>
