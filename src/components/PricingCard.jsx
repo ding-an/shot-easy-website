5c9266538c342a5c6b92709b2fb28b5ff8b81222
dingan <374619995@qq.com>
2025-02-11 10:37:48 +0800

    feat:接入Asiapay

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -6,6 +6,7 @@ import PayssionSelect from '@components/payssion/PayssionSelect'
 import useHandlers from './payssion/hooks/useHandlers'
 import { PAY_TYPE } from './payments/useOnerway'
 import PaymentNet from '@components/paymentNet'
+import Asiapay from '@components/Asiapay'
 
 const features = [
 	['Individual configuration', 'No setup, or hidden fees', 'Credits'],
@@ -284,20 +285,8 @@ export default function PricingCard() {
 						)}
 
 						<div className='flex flex-col gap-2 mt-auto'>
+							<Asiapay product={product} />
 							<PaymentNet product={product} />
-							{/* <a
-								href={`/pricing/onerway?id=${product.id}&type=${PAY_TYPE.card}`}
-								className='text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900'
-								onClick={() => {
-									const user = localStorage.getItem('user')
-									if (!user) {
-										const login = document.getElementById('login')
-										login.click()
-									}
-								}}
-							>
-								Pay with Debit/Credit Card
-							</a> */}
 							{/* <a
 								href={`/pricing/onerway?id=${product.id}&type=${PAY_TYPE.applepay}&name=${
 									product.name
